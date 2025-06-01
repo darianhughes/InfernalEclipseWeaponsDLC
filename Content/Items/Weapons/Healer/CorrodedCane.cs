a53aa31fca44ea3887a5589de94fba557d4bb051
George Feldy <georgefeldy@gmail.com>
2025-06-01 23:07:59 +0300

    dues flute WIP

--- Content/Items/Weapons/Healer/CorrodedCane.cs
+++ Content/Items/Weapons/Healer/CorrodedCane.cs
@@ -10,21 +10,16 @@ using ThoriumMod.Empowerments;
 using ThoriumMod.Items;
 using ThoriumMod.Sounds;
 
-namespace InfernalEclipseWeaponsDLC.Content.Items.Weapons.Bard
+namespace InfernalEclipseWeaponsDLC.Content.Items.Weapons.Healer
 {
-    public class NecroticChorus : BardItem
+    public class CorrodedCane : ThoriumItem
     {
-        public override BardInstrumentType InstrumentType => BardInstrumentType.Brass;
-
+        public override bool IsLoadingEnabled(Mod mod) => false;
         public override void SetStaticDefaults()
         {
-            Empowerments.AddInfo<Defense>(3);
-            Empowerments.AddInfo<Damage>(2);
-            Empowerments.AddInfo<LifeRegeneration>(2);
-            ItemID.Sets.ItemsThatAllowRepeatedRightClick[Type] = true;
         }
 
-        public override void SetBardDefaults()
+        public override void SetDefaults()
         {
             Item.damage = 175;
             Item.width = 28;
@@ -38,33 +33,18 @@ namespace InfernalEclipseWeaponsDLC.Content.Items.Weapons.Bard
             Item.value = CalamityGlobalItem.RarityYellowBuyPrice;
             Item.rare = ItemRarityID.Yellow;
             Item.UseSound = ThoriumSounds.Bard_Horn;
-            Item.shoot = ModContent.ProjectileType<NecroticChorusWisp>();
+            Item.shoot = ModContent.ProjectileType<CalamityMod.Projectiles.Boss.OldDukeVortex>();
             Item.shootSpeed = 1f;
-
-            InspirationCost = 5;
         }
-
-        public override bool AltFunctionUse(Player player) => true;
 
         public override bool CanShoot(Player player) => player.altFunctionUse == 2 || player.ownedProjectileCounts[Item.shoot] < 10;
 
         public override void UseStyle(Player player, Rectangle heldItemFrame)
         {
-            player.itemLocation.X -= 4 * player.direction;
-            player.itemLocation.Y -= 18;
-            player.itemRotation = 0;
-
         }
 
         public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
-            if (player.altFunctionUse == 2)
-            {
-                type = ModContent.ProjectileType<NecroticChorusPro>();
-                velocity *= 10f;
-                position.X += 38 * player.direction;
-                position.Y -= 18;
-            }
         }
     }
 }
---
+++ Content/Projectiles/BardPro/DeusFlutePro.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ThoriumMod;
+using ThoriumMod.Projectiles.Bard;
+
+namespace InfernalEclipseWeaponsDLC.Content.Projectiles.BardPro
+{
+    public class DeusFlutePro : BardProjectile
+    {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.None}";
+
+        public int ColorType
+        {
+            get => (int)Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 10;
+            ProjectileID.Sets.TrailingMode[Type] = 3;
+        }
+
+        public override void SetBardDefaults()
+        {
+            Projectile.aiStyle = -1;
+            Projectile.width = 8;
+            Projectile.height = 8;
+            Projectile.alpha = 255;
+            Projectile.penetrate = 1;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            ProjectileID.Sets.TrailCacheLength[Type] = 6;
+            ProjectileID.Sets.TrailingMode[Type] = 3;
+        }
+
+        private bool spawned;
+        public override void AI()
+        {
+            if (!spawned)
+            {
+                spawned = true;
+            }
+
+            if (Main.player[Projectile.owner].GetModPlayer<ThoriumPlayer>().accWindHoming)
+            {
+                for (int i = 0; i < Main.maxNPCs; i++)
+                {
+                    NPC npc = Main.npc[i];
+                    if (npc.CanBeChasedBy(Projectile.owner) && Vector2.DistanceSquared(npc.Center, Projectile.Center) < 600 * 600)
+                    {
+                        Vector2 vector = npc.Center - Projectile.Center;
+                        float num4 = Projectile.velocity.Length();
+                        vector.Normalize();
+                        vector *= num4;
+                        Projectile.velocity = (Projectile.velocity * 2 + vector) / 10f;
+                        Projectile.velocity.Normalize();
+                        Projectile.velocity *= num4;
+                        break;
+                    }
+                }
+
+            }
+
+            int dustType = ColorType == 1 ? ModContent.DustType<CalamityMod.Dusts.AstralBlue>() : ModContent.DustType<CalamityMod.Dusts.AstralOrange>();
+            for (int i = 0; i < Projectile.oldPos.Length; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.oldPos[i], Projectile.width, Projectile.height, dustType, Alpha: 0, Scale: Main.rand.NextFloat(0.4f, 1.2f));
+                dust.noGravity = true;
+                dust.noLight = false;
+                dust.rotation += Main.rand.NextFloat(MathHelper.TwoPi);
+                dust.velocity = default;
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+        }
+
+        public override void BardOnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Vector2 spawnOffset = new(Main.rand.NextFloat(-100, 100), Main.rand.NextFloat(-400, -200));
+            Vector2 position = target.Center + spawnOffset;
+            Vector2 velocity = new Vector2(20, 0).RotatedBy(new Vector2(-spawnOffset.X, -spawnOffset.Y).ToRotation());
+            Projectile.NewProjectileDirect(Projectile.GetSource_OnHit(target), position, velocity, ModContent.ProjectileType<DeusFluteStar>(), Projectile.damage, Projectile.knockBack, ai0: ColorType, ai1: target.whoAmI);
+        }
+    }
+}
