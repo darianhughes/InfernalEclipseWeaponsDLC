69c4b03c2b65e5f1f3b1550c4bdd1ec67fc8f77f
Joshua D'Abreu <JD250657@Falmouth.ac.uk>
2025-10-28 02:47:02 +0000

    Froststeel Gong first pass

---
+++ Content/Items/Weapons/Bard/FroststeelGong.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using ThoriumMod.Empowerments;
+using ThoriumMod;
+using ThoriumMod.Items;
+using ThoriumMod.Items.BardItems;
+using Microsoft.Xna.Framework;
+using InfernalEclipseWeaponsDLC.Content.Projectiles.BardPro.NukePros;
+using InfernalEclipseWeaponsDLC.Core.NewFolder;
+using ThoriumMod.Sounds;
+using CalamityMod.Items;
+using CalamityMod.Rarities;
+using CalamityMod.NPCs.Leviathan;
+using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.Items.Accessories;
+using InfernalEclipseWeaponsDLC.Content.Projectiles.BardPro;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+
+namespace InfernalEclipseWeaponsDLC.Content.Items.Weapons.Bard
+{
+    public class FroststeelGong : BigInstrumentItemBase
+    {
+        public override bool ForceDisableAutoReuse => true;
+
+        public override BardInstrumentType InstrumentType => BardInstrumentType.Percussion;
+
+        public override bool CanChangePitch => false;
+
+        public override void SafeSetStaticDefaults()
+        {
+            Empowerments.AddInfo<AquaticAbility>(2);
+            Empowerments.AddInfo<FlightTime>(2);
+            Empowerments.AddInfo<JumpHeight>(2);
+            Empowerments.AddInfo<MovementSpeed>(2);
+            Empowerments.AddInfo<InvincibilityFrames>(2);
+            Empowerments.AddInfo<ResourceMaximum>(2);
+            Empowerments.AddInfo<ResourceRegen>(2);
+        }
+
+        public override void SafeSetBardDefaults()
+        {
+            Item.useTime = 12;
+            Item.useAnimation = 12;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.noUseGraphic = false; // Hide item while using, same as SteelDrum
+            Item.holdStyle = 3;       // Use the same hold animation
+            Item.width = 30;
+            Item.height = 30;
+            Item.shootSpeed = 15;
+            Item.shoot = ModContent.ProjectileType<FroststeelPulse>(); // now the pulse
+            Item.autoReuse = false; // optional: single strike per use
+            Item.damage = 1000;
+            Item.knockBack = 4f;
+            Item.UseSound = ThoriumSounds.Cymbals;
+            Item.rare = ModContent.RarityType<Violet>();
+            Item.value = CalamityGlobalItem.RarityVioletBuyPrice;
+            InspirationCost = 10;
+        }
+
+        public override void UseItemFrame(Player player)
+        {
+            base.UseItemFrame(player);
+        }
+
+        public override void AddRecipes()
+        {
+            // Always get Thorium (we depend on it anyway)
+            Mod thorium = ModLoader.GetMod("ThoriumMod");
+            Mod calamity = null;
+            Mod clamity = null;
+            Mod ragnarok = null;
+
+            // Try to safely get Calamity and Ragnarok
+            ModLoader.TryGetMod("CalamityMod", out calamity);
+            ModLoader.TryGetMod("Clamity", out clamity);
+            ModLoader.TryGetMod("RagnarokMod", out ragnarok);
+
+            Recipe recipe = CreateRecipe();
+            if (ModLoader.TryGetMod("ThoriumMod", out Mod thoriumMod))
+            {
+                recipe.AddIngredient(thoriumMod.Find<ModItem>("FrostwindCymbals").Type, 1);
+                recipe.AddIngredient(thoriumMod.Find<ModItem>("SteelDrum").Type, 1);
+            }
+            if (clamity != null)
+            {
+                recipe.AddIngredient(clamity.Find<ModItem>("EnchantedMetal").Type, 8);
+            }
+            else
+            {
+                recipe.AddIngredient(calamity.Find<ModItem>("AuricBar").Type, 8);
+                recipe.AddIngredient(calamity.Find<ModItem>("EndothermicEnergy").Type, 20);
+            }
+            recipe.AddTile(ModContent.TileType<CosmicAnvil>());
+            recipe.Register();
+            base.AddRecipes();
+        }
+
+        public override void Shoot_OnSuccess(Player player)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Vector2 offset = Main.rand.NextVector2Circular(1f, 1f) * 20f;
+                Dust d = Dust.NewDustPerfect(player.Center + offset, DustID.Snow, offset.SafeNormalize(Vector2.Zero) * 50f, 100, default, 1.2f);
+                d.noGravity = true;
+            }
+            SoundEngine.PlaySound(SoundID.Item50, player.Center); // frosty chime sound
+        }
+
+        public override void SafeBardShoot(int success, int level, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position,
+    Vector2 velocity, int type, int damage, float knockback)
+        {
+            // Fire Froststeel Pulse directly from the player’s center
+            Projectile.NewProjectile(
+                source,
+                player.Center,
+                Vector2.Zero, // no movement, it grows in place
+                ModContent.ProjectileType<FroststeelPulse>(),
+                damage,
+                knockback,
+                player.whoAmI
+            );
+        }
+    }
+}
---
+++ Content/Projectiles/BardPro/FroststeelPulse.cs
@@ -0,0 +1,100 @@
+﻿using CalamityMod;
+using CalamityMod.Buffs.StatDebuffs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ThoriumMod;
+using ThoriumMod.Projectiles.Bard;
+
+namespace InfernalEclipseWeaponsDLC.Content.Projectiles.BardPro
+{
+    public class FroststeelPulse : BardProjectile
+    {
+        public override string Texture => "CalamityMod/Projectiles/Summon/RustyBeaconPulse";
+        public override BardInstrumentType InstrumentType => BardInstrumentType.Percussion;
+
+        public float LifetimeCompletion => 1f - (float)((ModProjectile)this).Projectile.timeLeft / 95f;
+
+        public override void SetBardDefaults()
+        {
+            ((Entity)((ModProjectile)this).Projectile).width = 96;
+            ((Entity)((ModProjectile)this).Projectile).height = 96;
+            ((ModProjectile)this).Projectile.friendly = true;
+            ((ModProjectile)this).Projectile.tileCollide = false;
+            ((ModProjectile)this).Projectile.ignoreWater = true;
+            ((ModProjectile)this).Projectile.penetrate = -1;
+            ((ModProjectile)this).Projectile.usesIDStaticNPCImmunity = true;
+            ((ModProjectile)this).Projectile.idStaticNPCHitCooldown = 30;
+            ((ModProjectile)this).Projectile.timeLeft = 95;
+            ((ModProjectile)this).Projectile.scale = 0.001f;
+        }
+
+        public override void AI()
+        {
+            if (((ModProjectile)this).Projectile.localAI[0] == 0f)
+            {
+                ((ModProjectile)this).Projectile.rotation = Utils.NextFloat(Main.rand, (float)Math.PI * 2f);
+                ((ModProjectile)this).Projectile.localAI[0] = Utils.ToDirectionInt(Utils.NextBool(Main.rand));
+                ((ModProjectile)this).Projectile.netUpdate = true;
+            }
+            ((ModProjectile)this).Projectile.Opacity = 1f - (float)Math.Pow((double)LifetimeCompletion, 1.56);
+            ((ModProjectile)this).Projectile.scale = MathHelper.Lerp(0.3f, 16f, LifetimeCompletion);
+            Projectile projectile = ((ModProjectile)this).Projectile;
+            projectile.rotation += ((ModProjectile)this).Projectile.localAI[0] * 0.012f;
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            // Icy white-blue gradient
+            Color start = new Color(180, 230, 255, 0);  // soft light blue
+            Color end = new Color(255, 255, 255, 128);  // fades to white glow
+            Color blended = Color.Lerp(start, end, LifetimeCompletion);
+
+            return blended * Projectile.Opacity;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[((ModProjectile)this).Projectile.type].Value;
+            Color drawColor = ((ModProjectile)this).Projectile.GetAlpha(lightColor) * 0.33f;
+            for (int i = 0; i < 8; i++)
+            {
+                float rotation = ((ModProjectile)this).Projectile.rotation;
+                Vector2 drawOffset = Utils.ToRotationVector2((float)Math.PI * 2f * (float)i / 8f) * ((ModProjectile)this).Projectile.scale;
+                Vector2 drawPosition = ((Entity)((ModProjectile)this).Projectile).Center - Main.screenPosition + drawOffset;
+                if (i % 2 == 1)
+                {
+                    rotation *= -1f;
+                }
+                Main.EntitySpriteDraw(texture, drawPosition, (Rectangle?)null, drawColor, rotation, Utils.Size(texture) * 0.5f, ((ModProjectile)this).Projectile.scale, (SpriteEffects)0, 0f);
+            }
+            return false;
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            return !target.CountsAsACritter && !target.friendly && target.chaseable;
+        }
+
+        public override void BardOnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.Frostburn, 120);
+            target.AddBuff(ModContent.BuffType<GlacialState>(), 60);
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return CalamityUtils.CircularHitboxCollision(((Entity)((ModProjectile)this).Projectile).Center, ((ModProjectile)this).Projectile.scale * 48f, targetHitbox);
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+    }
+}
