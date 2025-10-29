e8df29fb6893ed34f89eb5145869f92fd7313338
Joshua D'Abreu <JD250657@Falmouth.ac.uk>
2025-10-29 22:53:24 +0000

    Armor initial addition
    Defence amount is about right but otherwise currently mimicing existing armors for stats
    
    Eclipse - Umbraphile
    Super Cell - Shade Master
    Necrosinger - Maestro

---
+++ Content/Items/Armor/NecrosingerAnkles.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using ThoriumMod.Empowerments;
+using ThoriumMod;
+using ThoriumMod.Items;
+using ThoriumMod.Items.BardItems;
+using ThoriumMod.Utilities;
+using Microsoft.Xna.Framework;
+using InfernalEclipseWeaponsDLC.Content.Projectiles.BardPro.NukePros;
+using InfernalEclipseWeaponsDLC.Core.NewFolder;
+using ThoriumMod.Sounds;
+using CalamityMod.Items;
+using CalamityMod.Rarities;
+using CalamityMod.NPCs.Leviathan;
+using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.Items.Accessories;
+using InfernalEclipseWeaponsDLC.Content.Projectiles.BardPro;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using CalamityMod.Items.Materials;
+using CalamityMod.Items.Potions;
+
+namespace InfernalEclipseWeaponsDLC.Content.Items.Armor
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class NecrosingerAnkles : ModItem
+    {
+        public override void SetDefaults()
+        {
+            ((Entity)((ModItem)this).Item).width = 18;
+            ((Entity)((ModItem)this).Item).height = 18;
+            ((ModItem)this).Item.value = CalamityGlobalItem.RarityLimeBuyPrice;
+            ((ModItem)this).Item.rare = 7;
+            ((ModItem)this).Item.defense = 20;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            ref StatModifier damage = ref player.GetDamage((DamageClass)(object)ThoriumDamageBase<BardDamage>.Instance);
+            damage += 0.1f;
+            player.GetCritChance((DamageClass)(object)ThoriumDamageBase<BardDamage>.Instance) += 4f;
+            player.moveSpeed += 0.15f;
+        }
+
+        public override void AddRecipes()
+        {
+            Mod consolaria = null;
+
+            // Try to safely get Calamity and Ragnarok
+            ModLoader.TryGetMod("Consolaria", out consolaria);
+
+            Recipe recipe = CreateRecipe();
+
+            recipe.AddIngredient(ItemID.HallowedGreaves);
+            recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+            recipe.AddIngredient(ItemID.SoulofNight, 10);
+
+            if (ModLoader.TryGetMod("Consolaria", out Mod consolariaMod))
+            {
+                    recipe.AddIngredient(consolariaMod.Find<ModItem>("SoulofBlight").Type, 10);
+            }
+            else
+            {
+                recipe.AddIngredient<AureusCell>(10);
+                recipe.AddIngredient(ItemID.SoulofSight, 5);
+                recipe.AddIngredient(ItemID.SoulofMight, 5);
+                recipe.AddIngredient(ItemID.SoulofFright, 5);
+                    recipe.AddIngredient(ItemID.CursedFlame, 8);
+            }
+
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+
+            Recipe recipe2 = CreateRecipe();
+
+            recipe2.AddIngredient(ItemID.HallowedGreaves);
+            recipe2.AddIngredient(ItemID.TitaniumBar, 10);
+            recipe2.AddIngredient(ItemID.SoulofNight, 10);
+
+            if (ModLoader.TryGetMod("Consolaria", out Mod consolariaMod2))
+            {
+                recipe2.AddIngredient(consolariaMod2.Find<ModItem>("SoulofBlight").Type, 10);
+            }
+            else
+            {
+                recipe2.AddIngredient<AureusCell>(10);
+                recipe2.AddIngredient(ItemID.SoulofSight, 5);
+                recipe2.AddIngredient(ItemID.SoulofMight, 5);
+                recipe2.AddIngredient(ItemID.SoulofFright, 5);
+                recipe2.AddIngredient(ItemID.CursedFlame, 8);
+            }
+
+            recipe2.AddTile(TileID.MythrilAnvil);
+            recipe2.Register();
+
+            base.AddRecipes();
+        }
+    }
+}
