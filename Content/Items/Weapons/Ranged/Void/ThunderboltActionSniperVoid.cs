d02264056e610f9ced7397f5efc84fa9ebaab4a5
Ropro0923 <rcberry2010@gmail.com>
2026-02-16 21:31:25 -0600

    Created the Thunderbolt-Action Sniper

---
+++ Content/Items/Weapons/Ranged/Void/ThunderboltActionSniperVoid.cs
@@ -0,0 +1,71 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod.Items;
+using CalamityMod.Sounds;
+using CalamityMod.Rarities;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria.Audio;
+using InfernalEclipseWeaponsDLC.Content.Projectiles.RangedPro.Void;
+using SOTS.Void;
+
+namespace InfernalEclipseWeaponsDLC.Content.Items.Weapons.Ranged.Void
+{
+    [ExtendsFromMod("SOTS")]
+    public class ThunderboltActionSniperVoid : VoidItem
+    {
+        // Recipe needed
+        // Balance needed
+        // Needs a description
+        public override string Texture => "InfernalEclipseWeaponsDLC/Content/Items/Weapons/Ranged/Void/ThunderboltActionSniper";
+        public override bool IsLoadingEnabled(Mod mod) => ModLoader.HasMod("SOTS");
+        public override void SafeSetDefaults()
+        {
+            Item.damage = 900;
+            Item.DamageType = DamageClass.Ranged;
+            Item.useTime = 60;
+            Item.useAnimation = 60;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 6f;
+            Item.value = CalamityGlobalItem.RarityTurquoiseBuyPrice;
+            Item.rare = ModContent.RarityType<Turquoise>();
+            Item.autoReuse = false;
+            Item.shoot = ModContent.ProjectileType<VoidBolt>();
+            Item.shootSpeed = 6f;
+            Item.ammo = AmmoID.Bullet;
+            Item.width = 120;
+            Item.height = 34;
+        }
+        public override int GetVoid(Player player)
+        {
+            // I don't play void. I have no idea how much this should be.
+            return 1;
+        }
+        public override Vector2? HoldoutOffset() => new Vector2(-25, -2f);
+        public override void HoldItem(Player player) => player.scope = true;
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            // This is to prevent it from shooting through walls if you're too close, but I'm not sure how I like this.
+            // It was taken from Example Mod, so I don't really care if it get removed.
+            // Test, and if someone actually important to this mod happens, feel free to remove this chunk.
+            Vector2 muzzleOffset = Vector2.Normalize(velocity + new Vector2(0, -0.6f)) * 95f;
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            // So why does with just not increase volume?
+            SoundEngine.PlaySound(CommonCalamitySounds.LargeWeaponFireSound with { Volume = 10f }, player.position);
+            SoundEngine.PlaySound(SoundID.Thunder with { Volume = 10f }, player.position);
+
+            //    Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+
+            return true;
+        }
+    }
+}
---
+++ Content/Projectiles/RangedPro/Void/VoidBolt.cs
@@ -0,0 +1,70 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace InfernalEclipseWeaponsDLC.Content.Projectiles.RangedPro.Void
+{
+    public class VoidBolt : ModProjectile
+    {
+        // Balance needed
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+        public Player Owner => Main.player[Projectile.owner];
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = 6;
+            Projectile.timeLeft = 360;
+            Projectile.velocity *= 2f;
+            Projectile.extraUpdates = 25;
+            Projectile.DamageType = ModLoader.TryGetMod("SOTS", out Mod sots) ? sots.Find<DamageClass>("VoidRanged") : DamageClass.Ranged;
+        }
+        bool rising = true;
+        public override void AI()
+        {
+            /*
+            if (rising == true)
+            {
+                Projectile.ai[0]++;
+                if (Projectile.ai[0] >= 10)
+                {
+                    rising = false;
+                }
+            }
+            else
+            {
+                Projectile.ai[0]--;
+                if (Projectile.ai[0] <= -10)
+                {
+                    rising = true;
+                }
+            }
+            */
+           
+            if (rising && Projectile.ai[0]++ >= 10)
+            {
+                rising = false;
+            }
+            else if (!rising && Projectile.ai[0]-- <= -10)
+            {
+                rising = true;
+            }
+
+            for (int i = -1; i < 2; i++)
+            {
+            //    int dustNumber = Dust.NewDust(Projectile.position, 6, 6, DustID.Vortex, Projectile.velocity.X, Projectile.velocity.Y, 100);
+            //    Dust dust = Main.dust[dustNumber];
+                Dust dust = Main.dust[Dust.NewDust(Projectile.position, 6, 6, DustID.Vortex, Projectile.velocity.X, Projectile.velocity.Y, 100)];
+                dust.velocity = Vector2.Zero;
+                dust.position -= Projectile.velocity / 5f + new Vector2(0, 1 * Projectile.ai[0]);
+                dust.noGravity = true;
+                dust.scale = 1.3f;
+                dust.noLight = true;
+            }
+        }
+    }
+}
